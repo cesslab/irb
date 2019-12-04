677fb02520d115b03ad1e1e4b4e34ff5c46c50cb
aaruff <anwarruff@gmail.com>
2019-12-04 10:07:01 -0500

    Renamed view

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 
-from .views import ProjectListView, ProjectDetailView, ProjectCreateView
+from .views import ProjectListView, ProjectDetailView, ProjectResearcherCreateView
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project_list'),
     path('<int:id>', ProjectDetailView.as_view(), name='project_detail'),
-    path('create', ProjectCreateView.as_view(), name='project_create'),
+    path('create', ProjectResearcherCreateView.as_view(), name='project_create'),
 ]
