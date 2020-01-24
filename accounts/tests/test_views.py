e04d4662f89485d059cb2d4b735172556f7d21e6
aaruff <anwarruff@gmail.com>
2020-01-24 17:18:58 -0500

    created tests for user signup page

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,9 +1,13 @@
 from django.http import HttpRequest
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse
 
+from django.contrib.auth import get_user_model
 
-class SignUpTests(SimpleTestCase):
+User = get_user_model()
+
+
+class SignUpTests(TestCase):
 
     def test_status_code(self):
         response = self.client.get('/accounts/signup')
@@ -12,3 +16,13 @@ class SignUpTests(SimpleTestCase):
     def test_url_by_name(self):
         response = self.client.get(reverse('signup'))
 
+    def test_create_user(self):
+        data = {
+            'email': 'foofy@gmail.com',
+            'password1': 'foofyis1007b34f$',
+            'password2': 'foofyis1007b34f$'
+        }
+
+        response = self.client.post('/accounts/signup', data)
+        print(User.objects.first().email)
+        self.assertEqual(User.objects.count(), 1)
